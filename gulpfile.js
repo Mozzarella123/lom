421a3814d51e5996791e58409555835d685f27bc
Stryson <bairamov.samir99@gmail.com>
2018-10-14 17:40:04 +0300

    Squashed commit of the following:
    commit e722f1f056564205ca1b68886172b8e7d366b0aa
    Merge: 5d71597 6e25488
    Author: Рогов Никита <Nikita1998_2010@mail.ru>
    Date:   Sun Oct 14 17:27:44 2018 +0300
    
        Scheme block
        Merge branch 'Samir' of https://github.com/Mozzarella123/lom
    
        # Conflicts:
        #	app/sass/_libs.sass
    
    commit 5d71597fcd139360963a1465bf75ae225ec72e43
    Author: Рогов Никита <Nikita1998_2010@mail.ru>
    Date:   Fri Oct 12 23:44:48 2018 +0300
    
        Gulp settings

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,22 @@
 var syntax        = 'sass'; // Syntax: sass or scss;
 
 var gulp          = require('gulp'),
-		gutil         = require('gulp-util' ),
-		sass          = require('gulp-sass'),
-		browserSync   = require('browser-sync'),
-		concat        = require('gulp-concat'),
-		uglify        = require('gulp-uglify'),
-		cleancss      = require('gulp-clean-css'),
-		rename        = require('gulp-rename'),
-		autoprefixer  = require('gulp-autoprefixer'),
-		notify        = require("gulp-notify"),
-		rsync         = require('gulp-rsync');
+    gulpLoadPlugins  = require('gulp-load-plugins'),
+	plugins = gulpLoadPlugins({lazy: true, pattern : '*'});
+
+// gutil         = require('gulp-util' ),
+// 		sass          = require('gulp-sass'),
+// 		browserSync   = require('browser-sync'),
+// 		concat        = require('gulp-concat'),
+// 		uglify        = require('gulp-uglify'),
+// 		cleancss      = require('gulp-clean-css'),
+// 		rename        = require('gulp-rename'),
+// 		autoprefixer  = require('gulp-autoprefixer'),
+// 		notify        = require("gulp-notify"),
+// 		rsync         = require('gulp-rsync');
 
 gulp.task('browser-sync', function() {
-	browserSync({
+	plugins.browserSync({
 		server: {
 			baseDir: 'app'
 		},
@@ -26,12 +29,12 @@ gulp.task('browser-sync', function() {
 
 gulp.task('styles', function() {
 	return gulp.src('app/'+syntax+'/**/*.'+syntax+'')
-	.pipe(sass({ outputStyle: 'expanded' }).on("error", notify.onError()))
-	.pipe(rename({ suffix: '.min', prefix : '' }))
-	.pipe(autoprefixer(['last 15 versions']))
-	.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
+	.pipe(plugins.sass({ outputStyle: 'expanded' }).on("error", plugins.notify.onError()))
+	.pipe(plugins.rename({ suffix: '.min', prefix : '' }))
+	.pipe(plugins.autoprefixer(['last 15 versions']))
+	.pipe(plugins.cleanCss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
 	.pipe(gulp.dest('app/css'))
-	.pipe(browserSync.stream())
+	.pipe(plugins.browserSync.stream())
 });
 
 gulp.task('js', function() {
@@ -39,15 +42,15 @@ gulp.task('js', function() {
 		'app/libs/jquery/dist/jquery.min.js',
 		'app/js/common.js', // Always at the end
 		])
-	.pipe(concat('scripts.min.js'))
+	.pipe(plugins.concat('scripts.min.js'))
 	// .pipe(uglify()) // Mifify js (opt.)
 	.pipe(gulp.dest('app/js'))
-	.pipe(browserSync.reload({ stream: true }))
+	.pipe(plugins.browserSync.reload({ stream: true }))
 });
 
 gulp.task('rsync', function() {
 	return gulp.src('app/**')
-	.pipe(rsync({
+	.pipe(plugins.rsync({
 		root: 'app/',
 		hostname: 'username@yousite.com',
 		destination: 'yousite/public_html/',
@@ -63,7 +66,7 @@ gulp.task('rsync', function() {
 gulp.task('watch', ['styles', 'js', 'browser-sync'], function() {
 	gulp.watch('app/'+syntax+'/**/*.'+syntax+'', ['styles']);
 	gulp.watch(['libs/**/*.js', 'app/js/common.js'], ['js']);
-	gulp.watch('app/*.html', browserSync.reload)
+	gulp.watch('app/*.html', plugins.browserSync.reload)
 });
 
 gulp.task('default', ['watch']);
